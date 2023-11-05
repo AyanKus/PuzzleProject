1cc3258e8a137a191b5619831260cf61f33cec18
AyanKus <145852360+AyanKus@users.noreply.github.com>
2023-11-05 17:38:24 +0600

    Connected DB

--- GoLandProject/Project/PuzzleGame/cmd/api/puzzles.go
+++ GoLandProject/Project/PuzzleGame/cmd/api/puzzles.go
@@ -20,16 +20,12 @@ func (app *application) createPuzzleHandler(w http.ResponseWriter, r *http.Reque
 		app.badRequestResponse(w, r, err)
 		return
 	}
-	// Copy the values from the input struct to a new Movie struct.
 	puzzle := &data.Puzzle{
 		Title:        input.Title,
 		NumOfPuzzles: input.NumOfPuzzles,
 		Genres:       input.Genres,
 	}
-	// Initialize a new Validator.
 	v := validator.New()
-	// Call the ValidateMovie() function and return a response containing the errors if
-	// any of the checks fail.
 	if data.ValidateMovie(v, puzzle); !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
 		return
@@ -37,18 +33,12 @@ func (app *application) createPuzzleHandler(w http.ResponseWriter, r *http.Reque
 	fmt.Fprintf(w, "%+v\n", input)
 }
 
-// Add a showMovieHandler for the "GET /v1/movies/:id" endpoint. For now, we retrieve
-// the interpolated "id" parameter from the current URL and include it in a placeholder
-// response.
 func (app *application) showPuzzleHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
-	// Create a new instance of the Movie struct, containing the ID we extracted from
-	// the URL and some dummy data. Also notice that we deliberately haven't set a
-	// value for the Year field.
 	puzzle := data.Puzzle{
 		ID:           id,
 		CreatedAt:    time.Now(),
@@ -57,10 +47,8 @@ func (app *application) showPuzzleHandler(w http.ResponseWriter, r *http.Request
 		Genres:       []string{"drama", "romance", "war"},
 		Version:      1,
 	}
-	// Encode the struct to JSON and send it as the HTTP response.
 	err = app.writeJSON(w, http.StatusOK, envelope{"puzzle": puzzle}, nil)
 	if err != nil {
-		// Use the new serverErrorResponse() helper.
 		app.serverErrorResponse(w, r, err)
 	}
 }
---
+++ GoLandProject/Project/PuzzleGame/migrations/000002_add_puzzles_check_constraints.down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE puzzles DROP CONSTRAINT IF EXISTS puzzles_NumOfPuzzles_check;
+ALTER TABLE puzzles DROP CONSTRAINT IF EXISTS genres_length_check;
