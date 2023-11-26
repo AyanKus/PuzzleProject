01b3fbbdd42bab9a1fa9dcb089aa5be903802fb2
AyanKus <145852360+AyanKus@users.noreply.github.com>
2023-11-26 17:35:57 +0600

    Prepared Routes

--- GoLandProject/Project/PuzzleGame/cmd/api/routes.go
+++ GoLandProject/Project/PuzzleGame/cmd/api/routes.go
@@ -5,12 +5,16 @@ import (
 	"net/http"
 )
 
-func (app *application) routes() *httprouter.Router {
+func (app *application) routes() http.Handler {
 	router := httprouter.New()
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/puzzles", app.listPuzzlesHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/puzzles", app.createPuzzleHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/puzzles/:id", app.showPuzzleHandler)
-	return router
+	router.HandlerFunc(http.MethodPatch, "/v1/puzzles/:id", app.updatePuzzleHandler)
+	router.HandlerFunc(http.MethodDelete, "/v1/puzzles/:id", app.deletePuzzleHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+	return app.recoverPanic(app.rateLimit(router))
 }
