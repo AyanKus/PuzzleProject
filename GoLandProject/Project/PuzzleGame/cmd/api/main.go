1ba4d8c0d370052ff8cb6de8926d3a9261956c10
AyanKus <145852360+AyanKus@users.noreply.github.com>
2023-11-26 19:12:53 +0600

    Chapter 12

--- GoLandProject/Project/PuzzleGame/cmd/api/main.go
+++ GoLandProject/Project/PuzzleGame/cmd/api/main.go
@@ -6,9 +6,7 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"fmt"
 	_ "github.com/lib/pq"
-	"net/http"
 	"os"
 	"time"
 )
@@ -62,20 +60,12 @@ func main() {
 		logger: logger,
 		models: data.NewModels(db),
 	}
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
+	err = app.serve()
+	if err != nil {
+		logger.PrintFatal(err, nil)
 	}
-	logger.PrintInfo("starting server", map[string]string{
-		"addr": srv.Addr,
-		"env":  cfg.env,
-	})
-	err = srv.ListenAndServe()
-	logger.PrintFatal(err, nil)
 }
+
 func openDB(cfg config) (*sql.DB, error) {
 	db, err := sql.Open("postgres", cfg.db.dsn)
 	if err != nil {
